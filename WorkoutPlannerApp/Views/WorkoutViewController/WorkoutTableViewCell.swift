da299af5c4c534b994d14c8eae445c83612959b7
Kevin Vu <kevinvu59@gmail.com>
2019-12-18 09:03:14 -0500

    added a table view cell, a UIView (as a "card" view) and a label within the card view
    added outlets for the card UIView and the UILabel

---
+++ WorkoutPlannerApp/Views/WorkoutViewController/WorkoutTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WorkoutTableViewCell.swift
+//  WorkoutPlannerApp
+//
+//  Created by Kevin Vu on 12/18/19.
+//  Copyright © 2019 Kevin Vu. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var workoutCardView: UIView!
+    
+    @IBOutlet weak var workoutTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+}
