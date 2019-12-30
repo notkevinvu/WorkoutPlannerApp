c021ab88fdd969006077047139823c3cf7028bec
Kevin Vu <kevinvu59@gmail.com>
2019-12-29 19:41:11 -0500

    added another view controller that linked up to the button (ctrl drag from button to new view controller in storyboard and present modally)
    configured how the new view shows up after clicking the add workout button and configured color opacity so that you can see the elements below it as well

---
+++ WorkoutPlannerApp/Views/AddWorkoutViewController/AddWorkoutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddWorkoutViewController.swift
+//  WorkoutPlannerApp
+//
+//  Created by Kevin Vu on 12/29/19.
+//  Copyright © 2019 Kevin Vu. All rights reserved.
+//
+
+import UIKit
+
+class AddWorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
